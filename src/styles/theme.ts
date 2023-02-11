283a213548c3e586437d5e4c1fd5a048a42d96a3
Łukasz Markowski <lucas20193@gmail.com>
2023-02-11 19:56:48 +0100

    chore(app): basic configuration and theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    button: {
+      primary: "#000000",
+      secondary: "#ffffff",
+      hover: {
+        primary: "#444444",
+        secondary: "#bbbbbb",
+      },
+      disabled: {
+        primary: "#dedede",
+        border: "#aaaaaa",
+      },
+    },
+
+    primary: "#000000",
+    secondary: "#ffffff",
+    primaryLight: "#dddddd",
+    border: "#999999",
+  },
+};
